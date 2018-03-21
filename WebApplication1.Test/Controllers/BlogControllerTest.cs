cd2f93c02e682c313d642de83c47f14ffa547423
Vision <pepet@asynchrony.com>
2018-03-20 22:00:29 -0500

    10 - add test for 404 - tp

---
+++ WebApplication1.Test/Controllers/BlogControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Test.Controllers
+{
+    [TestClass]
+    public class BlogControllerTest
+    {
+        [TestClass]
+        public class Get : BlogControllerTest
+        {
+            [TestMethod]
+            public async Task ReturnsNotFound()
+            {
+                var getResult = await new BlogController().Get();
+
+                Assert.IsInstanceOfType(getResult, typeof(NotFoundResult));
+            }
+        }
+    }
+}
