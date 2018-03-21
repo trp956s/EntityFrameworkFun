4c1cfb0188d3a7418eb6a424b028ff5f1ec94ef9
Vision <pepet@asynchrony.com>
2018-03-20 22:04:22 -0500

    10.spike - backfill tests - tp

--- WebApplication1.Test/Controllers/BlogControllerTest.cs
+++ WebApplication1.Test/Controllers/BlogControllerTest.cs
@@ -8,15 +8,41 @@ namespace WebApplication1.Test.Controllers
     [TestClass]
     public class BlogControllerTest
     {
+        BlogController blogController = new BlogController();
+
+        [TestClass]
+        public class GetAll : BlogControllerTest
+        {
+            [TestMethod]
+            public async Task ReturnsNotFound()
+            {
+                var getResult = await blogController.Get();
+
+                Assert.IsInstanceOfType(getResult, typeof(NotFoundResult));
+            }
+        }
+
         [TestClass]
         public class Get : BlogControllerTest
         {
             [TestMethod]
             public async Task ReturnsNotFound()
             {
-                var getResult = await new BlogController().Get();
+                var getResult = await blogController.Get(0);
 
                 Assert.IsInstanceOfType(getResult, typeof(NotFoundResult));
+            }
+        }
+
+        [TestClass]
+        public class Post : BlogControllerTest
+        {
+            [TestMethod]
+            public async Task ReturnsNotFound()
+            {
+                var getResult = await blogController.Post(null);
+
+                Assert.IsInstanceOfType(getResult, typeof(BadRequestResult));
             }
         }
     }
