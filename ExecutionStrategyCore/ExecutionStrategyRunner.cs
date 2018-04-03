fa18c4a5061070f73bf5b7953187fb7ac03dc579
tom <trp956s@gmail.com>
2018-04-02 23:23:42 -0500

    s10.spike - wrap interfaces and class uses for base run strategy -tp

--- ExecutionStrategyCore/ExecutionStrategyRunner.cs
+++ ExecutionStrategyCore/ExecutionStrategyRunner.cs
@@ -12,15 +12,15 @@ namespace ExecutionStrategyCore
             return await executionStrategy.Run();
         }
 
-        public async Task<T> Run<T>(InternalRunnerWrapper<Task<T>> wrapper)
+        public async Task<T> Run<T>(IRunner<Task<T>> executionWrapper)
         {
-            return await wrapper.Runner.Run();
+            return await executionWrapper.Run();
         }
     }
 
     public interface ITaskRunner //will replace IExecutionStrategyRunner
     {
-        Task<T> Run<T>(InternalRunnerWrapper<Task<T>> wrapper);
+        Task<T> Run<T>(IRunner<Task<T>> wrapper);
     }
 
     public interface IMapper<T1, T2>
@@ -28,12 +28,12 @@ namespace ExecutionStrategyCore
         T2 Run(T1 arg);
     }
 
-    public interface IQuerySingle<DbSetType> : IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<DbSetType>>> { }
+    public interface IQuery<DbSetType, ReturnType> : IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<ReturnType>>> { }
 
     public class DbSet<T>
     { }
 
-    public struct GetAll : IQuerySingle<int>
+    public struct GetAll : IQuery<int, int>
     {
         public InternalRunnerWrapper<Task<int>> Run(DbSet<int> arg)
         {
@@ -57,12 +57,12 @@ namespace ExecutionStrategyCore
 
     public interface IDbSetRunner<DbSetType>
     {
-        Task<T> Run<T>(IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<T>>> ga);
+        Task<T> Run<T>(IQuery<DbSetType, T> ga);
     }
 
     public static class DbSetRunner<DbSetType>
     {
-        public static async Task<T> Run<T>(ITaskRunner runner, DbSet<DbSetType> dbSet, IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<T>>> ga)
+        public static async Task<T> Run<T>(ITaskRunner runner, DbSet<DbSetType> dbSet, IQuery<DbSetType, T> ga)
         {
             return await runner.Run(ga.Run(dbSet));
         }
@@ -80,7 +80,7 @@ namespace ExecutionStrategyCore
             this.runner = runner;
         }
 
-        public async Task<T> Run<T>(IMapper<DbSet<int>, InternalRunnerWrapper<Task<T>>> ga)
+        public async Task<T> Run<T>(IQuery<int, T> ga)
         {
             return await DbSetRunner<int>.Run(runner, dbSet, ga);
         }
---
+++ ExecutionStrategyCore/ITaskRunnerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ExecutionStrategyCore
+{
+    public static class ITaskRunnerExtensions
+    {
+        public static async Task<T> Run<T>(this ITaskRunner runner, InternalRunnerWrapper<Task<T>> internalWrapper)
+        {
+            return await runner.Run<T>(internalWrapper.Runner);
+        }
+    }
+}
