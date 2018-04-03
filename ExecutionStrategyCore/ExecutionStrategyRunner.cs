fd884b2c255424a5177db3645a806c8e3b1b00db
tom <trp956s@gmail.com>
2018-04-02 23:10:19 -0500

    s10.spike - support tasks, simplify interfaces, start using framework - tp

--- ExecutionStrategyCore/ExecutionStrategyRunner.cs
+++ ExecutionStrategyCore/ExecutionStrategyRunner.cs
@@ -5,87 +5,84 @@ using System.Threading.Tasks;
 
 namespace ExecutionStrategyCore
 {
-    public class ExecutionStrategyRunner : IExecutionStrategyRunner
+    public class ExecutionStrategyRunner : IExecutionStrategyRunner, ITaskRunner
     {
         public async Task<T> Run<T>(ExecutionStrategy<T> executionStrategy)
         {
             return await executionStrategy.Run();
         }
 
-        public interface IAnyRunner
+        public async Task<T> Run<T>(InternalRunnerWrapper<Task<T>> wrapper)
         {
-            T Run<T>(InternalRunnerWrapper<T> wrapper);
+            return await wrapper.Runner.Run();
+        }
+    }
+
+    public interface ITaskRunner //will replace IExecutionStrategyRunner
+    {
+        Task<T> Run<T>(InternalRunnerWrapper<Task<T>> wrapper);
+    }
+
+    public interface IMapper<T1, T2>
+    {
+        T2 Run(T1 arg);
+    }
+
+    public interface IQuerySingle<DbSetType> : IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<DbSetType>>> { }
+
+    public class DbSet<T>
+    { }
+
+    public struct GetAll : IQuerySingle<int>
+    {
+        public InternalRunnerWrapper<Task<int>> Run(DbSet<int> arg)
+        {
+            return new InternalRunnerWrapper<Task<int>>(new ValueCacheRunner<Task<int>>(Task.FromResult(0)));
+        }
+    }
+
+    public struct ValueCacheRunner<T> : IRunner<T>
+    {
+        private readonly T value;
+
+        public ValueCacheRunner(T value) {
+            this.value = value;
         }
 
-        public interface IMapper<T1, T2>
+        public T Run()
         {
-            T2 Run(T1 arg);
+            return value;
+        }
+    }
+
+    public interface IDbSetRunner<DbSetType>
+    {
+        Task<T> Run<T>(IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<T>>> ga);
+    }
+
+    public static class DbSetRunner<DbSetType>
+    {
+        public static async Task<T> Run<T>(ITaskRunner runner, DbSet<DbSetType> dbSet, IMapper<DbSet<DbSetType>, InternalRunnerWrapper<Task<T>>> ga)
+        {
+            return await runner.Run(ga.Run(dbSet));
+        }
+    }
+
+    public struct DbBlogSetRunner : IDbSetRunner<int>
+    {
+        //inject dbset and runner
+        private readonly DbSet<int> dbSet;
+        private readonly ITaskRunner runner;
+
+        public DbBlogSetRunner(DbSet<int> dbSet, ITaskRunner runner)
+        {
+            this.dbSet = dbSet;
+            this.runner = runner;
         }
 
-        public class DbSet<T>
-        { }
-
-        public struct GetAll : IMapper<DbSet<int>, InternalRunnerWrapper<int>>
+        public async Task<T> Run<T>(IMapper<DbSet<int>, InternalRunnerWrapper<Task<T>>> ga)
         {
-            public InternalRunnerWrapper<int> Run(DbSet<int> arg)
-            {
-                return new InternalRunnerWrapper<int>(new ValueCacheRunner<int>(0));
-            }
-        }
-
-        private struct ValueCacheRunner<T> : IRunner<T>
-        {
-            private readonly T value;
-
-            public ValueCacheRunner(T value) {
-                this.value = value;
-            }
-
-            public T Run()
-            {
-                return value;
-            }
-        }
-
-
-        public interface IDbSetRunner<DbSetType>
-        {
-            T Run<T>(IMapper<DbSet<DbSetType>, InternalRunnerWrapper<T>> ga);
-        }
-
-        public class DbSetRunner<DbSetType> : IDbSetRunner<DbSetType>
-        {
-            private readonly DbSet<DbSetType> dbSet;
-            private readonly IAnyRunner runner;
-
-            public DbSetRunner(DbSet<DbSetType> dbSet, IAnyRunner runner)
-            {
-                this.dbSet = dbSet;
-                this.runner = runner;
-            }
-
-            public T Run<T>(IMapper<DbSet<DbSetType>, InternalRunnerWrapper<T>> ga)
-            {
-                return runner.Run(ga.Run(dbSet));
-            }
-        }
-
-        public class DbBlogSetRunner : IDbSetRunner<int>
-        {
-            private readonly DbSet<int> dbSet;
-            private readonly IRunner<IDbSetRunner<int>> dbSetRunnerFactory;
-
-            //inject dbset
-            public DbBlogSetRunner(DbSet<int> dbSet, IRunner<IDbSetRunner<int>> dbSetRunnerFactory)
-            {
-                this.dbSet = dbSet;
-                this.dbSetRunnerFactory = dbSetRunnerFactory;
-            }
-
-            public T Run<T>(IMapper<DbSet<int>, InternalRunnerWrapper<T>> ga)
-            {
-                return dbSetRunnerFactory.Run().Run(ga);
-            }
+            return await DbSetRunner<int>.Run(runner, dbSet, ga);
         }
     }
 }
